04b06fc011b97f0a3868eaee496f1d924605beec
imj <imijij25@gamil.com>
2023-08-05 16:25:39 +0900

    테스트

---
+++ src/kr/bit/controller/MemberLoginController.java
@@ -0,0 +1,45 @@
+package kr.bit.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import kr.bit.model.MemberDAO;
+import kr.bit.model.MemberVO;
+
+public class MemberLoginController implements Controller{
+
+	@Override
+	public String requestHandler(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		String ctx=request.getContextPath(); // /MVC06
+		
+		String user_id=request.getParameter("user_id");
+		String password=request.getParameter("password");
+		System.out.println("user_id : "+user_id+ "password: "+password);
+		MemberVO vo = new MemberVO();
+		vo.setId(user_id);
+		vo.setPass(password);
+		
+		MemberDAO dao=new MemberDAO();
+		System.out.println("!!!"+vo.toString());
+		String user_name=dao.memberLogin(vo);
+		if(user_name!=null&& !"".equals(user_name)) {
+			HttpSession session=request.getSession();
+			session.setAttribute("userId", user_id);
+			session.setAttribute("userName", user_name);
+		}else {
+			System.out.println("111122222");
+			request.getSession().setAttribute("userId", "");
+			request.getSession().setAttribute("userName", "");
+			request.getSession().setAttribute("msg", "사용자 정보가 올바르지 않습니다");
+		}
+		
+		return "redirect:"+ctx+"/memberList.do";
+	}
+
+}
